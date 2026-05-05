b7d9107f18e341264f7b9f1d9eedcb50159c2d4d
Fady Philip <fadyph2003@gmail.com>
2026-05-05 18:06:55 +0300

    implement the directory creation with init and cat-file to read files into a string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
-fn main() {
-    println!("Hello, world!");
+mod object;
+
+use std::env;
+use std::fs;
+
+// use crate::object::compress_blob;
+// use crate::object::create_blob;
+// use crate::object::decompress_blob;
+// use crate::object::hash_blob;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Reads the input arguments ex: git commit path/to/file.md
+    // Note: args[0] is the path to this project , it is
+    // target/debug/git-rs
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: git-rs <command> [<args>]");
+        std::process::exit(1);
+    }
+
+    run(&args)
+}
+
+fn run(args: &Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
+    match args[1].as_str() {
+        "init" => {
+            if args.len() != 2 {
+                eprintln!("Usage git-rs init");
+                std::process::exit(1);
+            }
+            create_dir_structure()?;
+            Ok(())
+        }
+        "cat-file" => {
+            if args.len() < 3 {
+                eprintln!("Usage: git-rs cat-file <file>");
+                std::process::exit(1)
+            }
+            let content = read_file(args[2].as_str())?;
+            println!("{}", content);
+            Ok(())
+        }
+        "write-tree" => Ok(()),
+        "hash" => {
+            if args.len() < 3 {
+                eprintln!("Usage git-rs hash <file>");
+                std::process::exit(1)
+            }
+            let file = read_file(&args[2]);
+
+            // let hashed_blob = hash_blob(blob);
+
+            println!("{:?}", file);
+            Ok(())
+        }
+        unknown => {
+            eprintln!("Unknown Command : {}", unknown);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn read_file(path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let contents = fs::read_to_string(path)?;
+    Ok(contents)
+}
+
+fn create_dir_structure() -> Result<(), Box<dyn std::error::Error>> {
+    fs::create_dir_all(".git/objects/info")?;
+    fs::create_dir_all(".git/objects/pack/")?;
+    fs::create_dir_all(".git/refs/heads/")?;
+    fs::create_dir_all(".git/refs/tags/")?;
+    fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
+    Ok(())
 }
